01d777ded000c6b9edd8d2fa25634787e91be9db
baesuhan <105197524+uh004@users.noreply.github.com>
2022-12-04 01:31:12 +0900

    Create StudentBean.java

---
+++ project/StudentBean.java
@@ -0,0 +1,35 @@
+package homework;
+
+public class StudentBean {
+	
+	private String sub;
+	private String name;
+	private String number;
+	private String subject;
+	public String getSub() {
+		return sub;
+	}
+	public void setSub(String sub) {
+		this.sub = sub;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	
+}
