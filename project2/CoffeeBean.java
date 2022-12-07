efe02d9cc285904c996f14e8e326f89b223e1931
baesuhan <105197524+uh004@users.noreply.github.com>
2022-12-07 22:49:33 +0900

    Create CoffeeBean.java

---
+++ project2/CoffeeBean.java
@@ -0,0 +1,29 @@
+package homework2;
+
+public class CoffeeBean {
+
+	private String coffee;
+	private String size;
+	private String count;
+	
+	public String getCoffee() {
+		return coffee;
+	}
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getCount() {
+		return count;
+	}
+	public void setCount(String count) {
+		this.count = count;
+	}
+	
+	
+}
